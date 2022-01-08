0233b86b4fc0c07213f2abca41416c95957b5b77
Dharmendra <gohildharmendrab@gmail.com>
2022-01-08 14:27:05 +0530

    Offline comm

---
+++ MyDesign/footer.php
@@ -0,0 +1,7 @@
+  <!-- FOOTER -->
+  <footer class="container">
+      <p class="float-end"><a href="#">Back to top</a></p>
+      <p>&copy; 2022 TechnicalDB, Inc. &middot;
+          <!-- <a href="#">Privacy</a> &middot; <a href="#">Terms</a> -->
+      </p>
+  </footer>
---
+++ MyDesign/header_main.php
@@ -0,0 +1,49 @@
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="./">B-v5.1.3</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ms-auto mb-2 mb-md-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./album.php">Album</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./blog.php">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./carousel.php">Carousel</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./checkout-form.php">Checkout-Form</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./cover.php">Cover</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./dashboard.php">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./dropdowns.php">Dropdowns</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./features.php">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./footers.php">Footers</a>
+                    </li>
+                </ul>
+                <!-- <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form> -->
+            </div>
+        </div>
+    </nav>
+</header>
